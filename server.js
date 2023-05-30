708ea769ac5b16f6802ec3b824c650954a90d320
Ishay (server) <ishay.yemini@gmail.com>
2023-05-30 08:47:41 +0000

    Added /email-logs api

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
-const sql = require('mssql')
 const cors = require('cors')
-const compression = require('compression')
-
-const getReportData = require('./get_report_data')
+const nodemailer = require('nodemailer')
 
 const app = express()
 
 app.use(cors())
-app.use(compression())
+app.use(express.text({ limit: '10mb' }))
+
+const emailRegex =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'yieldx.dev@gmail.com',
+    pass: 'qxkaaajeobfflkph',
+  },
+})
 
 app.get('/test', (req, res) => {
   res.send('test ok!')
@@ -18,34 +26,34 @@ app.get('/', (req, res) => {
   res.send('welcome to the backend (:')
 })
 
-app.get('/get-report-data', (req, res) => {
-  const { UID, username } = req.query
-  if (!UID) res.json({ err: 'Missing UID' })
-  else {
-    res.writeHead(200, {
-      Connection: 'keep-alive',
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache',
-    })
-    getReportData({ UID, username }, res).catch((err) => {
-      console.log(err)
-    })
-  }
+app.post('/email-logs', (req, res) => {
+  const content = req.body
+  const { email, uid } = req.query
+  const to = emailRegex.test(email) ? email : 'amit@yieldx.biz'
+  if (!content) res.send('error: no content sent')
+  else
+    transporter
+      .sendMail({
+        from: 'yieldx.dev@gmail.com',
+        to,
+        subject: 'RedMite Log ' + (uid ? uid : ''),
+        attachments: [
+          {
+            filename: `redmite_log_${uid ? uid + '_' : ''}${Date.now()}.txt`,
+            content,
+          },
+        ],
+      })
+      .then((result) => {
+        res.send(`ok: sent log to ${to}`)
+        console.log('Mail sent:', result.response)
+      })
+      .catch((err) => {
+        res.send('error: ' + err.message)
+        console.log(err.message)
+      })
 })
 
-const config = {
-  user: 'sa',
-  password: 'Yieldxbiz2021',
-  server: 'localhost',
-  database: 'SensorsN',
-  options: { encrypt: false },
-}
-sql.connect(config).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log('Server Running on PORT', process.env.PORT)
-  })
-})
-
-sql.on('error', (err) => {
-  console.log(err)
+app.listen(process.env.PORT, () => {
+  console.log('Server Running on PORT', process.env.PORT)
 })
